abc65ffa0c54ee4cd6003d06a839014bb9130607
huitzylihuitl <luisescobar@pop-os.localdomain>
2019-10-01 15:27:43 -0500

    Changes to main script, adding some more plots

--- Main_Reducibility.R
+++ Main_Reducibility.R
@@ -30,6 +30,10 @@ map(.x = paste0(source_files_path, source.files), .f = source)
 multicapa_temporal <- readRDS(path_to_multiplex)
 
 
+# Number of layers
+no_layers <- length(multicapa_temporal[[1]]) - 1
+names_layers <- names(multicapa_temporal[[1]])
+names_layers <- names_layers[-length(names_layers)]
 
 # Perform optimization for each date --------------------------------------
 
@@ -81,16 +85,22 @@ ggsave(filename = "HistoricalReducibility.pdf", plot = red_plot, device = "pdf",
 
 
 
+#   -----------------------------------------------------------------------
 # Calculate structural metrics of multiplex -------------------------------
+#   -----------------------------------------------------------------------
 
-grados_temporales <- map(.x = multicapa_temporal, .f = calculate_multiplex_degree, directed = F)
+
+# Multiplex degree --------------------------------------------------------
+grados_temporales <- map(.x = multicapa_temporal,
+                         .f = calculate_multiplex_degree,
+                         directed = F)
 
 
 grados_temporales_df <- grados_temporales %>%
     bind_rows(.id = "Fecha")
 grados_temporales_df$Fecha <- grados_temporales_df$Fecha %>% as.Date()
 
-grados_temporales_df %>%
+plot_degrees_temporal_by_layer <- grados_temporales_df %>%
     gather(key = Capa, value = Grado, -Fecha, -Node) %>%
     filter(Capa != "Agregada",
            Capa != "OverlappingDeg") %>%
@@ -119,10 +129,103 @@ grados_temporales_df %>%
     scale_x_date(date_breaks = "3 months", date_labels = format("%Y-%m")) +
     labs(title = "20-day simple moving average of max, min and mean degree per layer",
          subtitle = "Number of connections")
-    
+
+ggsave(filename = "HistoricalDegreeByLayer.pdf",
+       plot = plot_degrees_temporal_by_layer, device = "pdf",
+       path = path_to_write_output, scale = 2, width = 19, height = 9.5,
+       units = "cm", dpi = 300)    
 
 
 
-# Node activity from degrees ----------------------------------------------
 
-node_activity <- grados_temporales_df %>% select(Fecha, Node, starts_with("Capa"))
+
+
+
+# Node activity -----------------------------------------------------------
+node_activity <- grados_temporales_df %>%
+    select(-Agregada, -OverlappingDeg) %>%
+    gather(key = Capa, value = Valor, -Fecha, -Node) %>%
+    mutate(Activo = if_else(Valor > 0, 1, 0))
+
+node_activity <- node_activity %>%
+    select(-Valor) %>%
+    spread(key = Capa, value = Activo)
+
+
+
+# Active nodes per layer --------------------------------------------------
+actives_nodes_per_layer <- node_activity %>% 
+    gather(key = Capa, value = Status, -Fecha, -Node) %>%
+    group_by(Fecha, Capa) %>%
+    summarize(NoActiveNodes = sum(Status, na.rm = T)) %>%
+    ggplot(aes(x = Fecha, y = NoActiveNodes, group = Capa, colour = Capa)) +
+    geom_ma(ma_fun = SMA, n = 20, linetype = "solid") +
+    theme_bw() +
+    theme(axis.title = element_blank(),
+          legend.position = "bottom",
+          legend.text = element_text(size = 13),
+          legend.title = element_blank(),
+          axis.text = element_text(size = 13),
+          axis.text.x = element_text(angle = 90),
+          strip.text = element_text(size = 12, face = "bold"),
+          plot.title = element_text(size = 16, face = "bold"),
+          plot.subtitle = element_text(size = 14)) +
+    scale_color_viridis_d(option = "D") +
+    scale_x_date(date_breaks = "3 months", date_labels = format("%Y-%m")) +
+    labs(title = "20-days moving average of the number of active banks per layer",
+         subtitle = "Number of institutions")
+
+ggsave(filename = "NoActivesNodesPerLayer.pdf", plot = actives_nodes_per_layer,
+       device = "pdf", path = path_to_write_output, scale = 2, width = 19,
+       height = 9.5, units = "cm", dpi = 400)
+
+
+
+# Active layers per node --------------------------------------------------
+
+active_layers_per_node <- node_activity %>%
+    gather(key = Capa, value = Status, -Fecha, -Node) %>% 
+    group_by(Fecha, Node) %>%
+    summarize(NoActiveLayers = sum(Status, na.rm = T)) %>%
+    ggplot(aes(x = Fecha, y = NoActiveLayers, group = Node, colour = Node)) +
+    geom_ma(ma_fun = SMA, n = 20, linetype = "solid") +
+    geom_vline(xintercept = as.Date("2018-07-02"), colour = "red",
+               linetype = "longdash") +
+    theme_bw() +
+    theme(legend.position = "none",
+          axis.title = element_blank(),
+          legend.text = element_text(size = 13),
+          legend.title = element_blank(),
+          axis.text = element_text(size = 13),
+          axis.text.x = element_text(angle = 90),
+          strip.text = element_text(size = 12, face = "bold"),
+          plot.title = element_text(size = 16, face = "bold"),
+          plot.subtitle = element_text(size = 14)) +
+    facet_wrap(~Node, scales = "free_y") +
+    scale_x_date(date_breaks = "3 months", date_labels = format("%Y-%m")) +
+    scale_color_viridis_d(option = "D") +
+    labs(title = "20-days moving average of the number of active layers per node",
+         subtitle = "Number of markets")
+
+ggsave(filename = "NoActiveLayersPerNode.pdf", plot = active_layers_per_node,
+       device = "pdf", path = path_to_write_output, scale = 2, width = 19,
+       height = 9.5, units = "cm", dpi = 400)
+
+
+
+
+
+# Layer participation for each node ---------------------------------------
+layer_participation_coefficient_per_node <- grados_temporales_df %>%
+    select(-Agregada) %>%
+    mutate_at(vars(-c(Fecha, Node, OverlappingDeg)),
+              funs(./OverlappingDeg))
+
+
+layer_participation_coefficient_per_node <- layer_participation_coefficient_per_node %>%
+    select(-OverlappingDeg) %>%
+    mutate_at(vars(-c(Fecha, Node)), funs(.^(2)))
+
+
+layer_participation_coefficient_per_node <- layer_participation_coefficient_per_node %>%
+    mutate(SumaAux = rowSums(select(layer_participation_coefficient_per_node, -Fecha, -Node)))
